30092b01bd4997bf1099b7b62bb12110249ed6ae
postwo <codo7717@naver.com>
2025-05-22 00:24:52 +0900

    swagger 이슈 발생 RestControllerAdvice를 붙이면 403 에러가 발생 일단 @Hidden을 붙여줌으로써 에러 해결

--- src/main/java/com/onion/backend/handler/GlobalExceptionHandler.java
+++ src/main/java/com/onion/backend/handler/GlobalExceptionHandler.java
@@ -1,12 +1,13 @@
 package com.onion.backend.handler;
 
 import com.onion.backend.exception.ResourceNotFoundException;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-
+@Hidden
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
