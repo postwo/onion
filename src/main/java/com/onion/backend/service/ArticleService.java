a10d746b204af888aa88a80bc67691f77bf491fe
postwo <codo7717@naver.com>
2025-05-22 22:25:09 +0900

    댓글 수정 기능 구현

--- src/main/java/com/onion/backend/service/ArticleService.java
+++ src/main/java/com/onion/backend/service/ArticleService.java
@@ -142,6 +142,9 @@ public class ArticleService {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         Article latestArticle = articleRepository.findLatestArticleByAuthorUsernameOrderByCreatedDate(userDetails.getUsername());
+        if (latestArticle == null) {
+            return true;
+        }
         return this.isDifferenceMoreThanFiveMinutes(latestArticle.getCreatedDate());
     }
 
@@ -149,6 +152,9 @@ public class ArticleService {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         Article latestArticle = articleRepository.findLatestArticleByAuthorUsernameOrderByUpdatedDate(userDetails.getUsername());
+        if (latestArticle == null || latestArticle.getUpdatedDate() == null) {
+            return true;
+        }
         return this.isDifferenceMoreThanFiveMinutes(latestArticle.getUpdatedDate());
     }
 
